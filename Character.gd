b3e769fd455498661e417a4f9ffcf9a9284abea2
Brooklin Myers <brooklinjazzmyers@gmail.com>
2020-09-29 22:17:20 -0700

    Setup basic player movement and animation

--- Character.gd
+++ Character.gd
@@ -32,18 +32,15 @@ var motion = Vector2()
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 func direction_factory():
 	if (Input.is_action_pressed("right") and Input.is_action_pressed("left")):
-		return  Direction.CENTER
+		return Direction.CENTER
 	elif (Input.is_action_pressed("left")):
 		return Direction.LEFT
 	elif (Input.is_action_pressed("right")):
 		return Direction.RIGHT
+	else:
+		return Direction.CENTER
 
 func movement_factory():
 	if (Input.is_action_pressed("jump")):
@@ -64,12 +61,13 @@ func airborn_factory():
 func state_factory():
 	return {
 		"airborn": airborn_factory(),
-		"direction":direction_factory(),
+		"direction": direction_factory(),
 		"movement": movement_factory()
 	}
 
-const GRIP_SPEED = 100
-const WALK_SPEED = 200	
+const GRIP_SPEED = 100 * 50
+const WALK_SPEED = 200 * 50
+const GRAVITY = 400
 
 func get_speed(movement):
 	if (movement == Movement.GRIP):
@@ -80,38 +78,56 @@ func get_speed(movement):
 func walk(state, delta):
 	var speed = get_speed(state.movement)
 	if (state.direction == Direction.LEFT):
-		motion.x = speed * delta
-	elif (state.direction == Direction.RIGHT):
 		motion.x = -speed * delta
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	elif (state.direction == Direction.RIGHT):
+		motion.x = speed * delta
+	else:
+		motion.x = 0
+		
+func gravity(state, delta):
+	if (state.airborn == Airborn.ON_FLOOR):
+		pass
+	else:
+		motion.y += GRAVITY * delta
+		
+
 func _process(delta):
 	var state = state_factory()
 	walk(state, delta)
+	gravity(state, delta)
 	move_and_slide(motion, Vector2.UP)
-#	animate()
-	
-func animate():
-	if (Input.is_action_pressed("jump")):
-		$Legs.play("charge_jump")
-		$Body.play("charge_jump")
-	elif (Input.is_action_pressed("grip") and Input.is_action_pressed("right")):
-		$Legs.play("grip")
-		$Body.play("grip")
-	elif (Input.is_action_pressed("grip") and Input.is_action_pressed("left")):
-		$Legs.play("grip", true)
-		$Body.play("grip")
-	elif ($Legs.animation == "walk" or $Legs.animation == "grip") and $Legs.frame != 0:
-		print($Legs.frame)
-		pass
-	elif (Input.is_action_pressed("grip")):
-		$Legs.play("grip_idle")
-		$Body.play("grip")
-	elif (Input.is_action_pressed("right")):
-		$Legs.play("walk")
-		$Body.play("idle")
-	elif (Input.is_action_pressed("left")):
-		$Legs.play("walk", true)
-		$Body.play("idle")
-	else:
-		$Body.play("idle")
-		$Legs.play("idle")
+	animate(state)
+
+func animate(state):
+	if (state.direction == Direction.LEFT):
+		$Body.flip_h = true
+	elif (state.direction == Direction.RIGHT):
+		$Body.flip_h = false
+	match state.movement:
+		Movement.WALK:
+			$Body.set_animation("default")
+			$Legs.set_animation("walk")
+			pass
+		Movement.GRIP:
+			$Body.set_animation("grip")
+			$Legs.set_animation("grip")
+		Movement.JUMP_CHARGE:
+			$Legs.set_animation("charge_jump")
+			$Body.set_animation("charge_jump")
+#	set animation defaults to start playing for some reason
+	$Legs.stop()	
+
+#	TODO - we probably have to make this work with delta to make frames match movement
+	var numberOfFrames = $Legs.get_sprite_frames().get_frame_count($Legs.get_animation())
+	match state.direction:
+		Direction.CENTER:
+			pass
+		Direction.LEFT:
+			#	TODO - figure out how to improve logic
+			if ($Legs.get_frame() == 0):
+				$Legs.set_frame(7)
+			else:
+				$Legs.set_frame(($Legs.get_frame() - 1))		
+		Direction.RIGHT:
+			$Legs.set_frame(($Legs.get_frame() + 1) % numberOfFrames)
+			
