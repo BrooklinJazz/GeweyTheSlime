f52234a2c449303aa0f9b0bcd26ee45397e5c325
Micah Peacock <35236977+MicahPeacock@users.noreply.github.com>
2020-10-20 21:21:17 -0600

    Updated functions in GroundMovement towards a more immutable paradigm

--- GroundMovement.gd
+++ GroundMovement.gd
@@ -5,7 +5,7 @@ extends Node
 const GRIP_SPEED = 200
 const WALK_SPEED = 400
 const GRAVITY = 400
-const JUMP_FORCE = 1000 * 15
+const JUMP_FORCE = -250
 
 class_name GroundMovement
 
@@ -13,24 +13,22 @@ var character
 func _init(parent):
 	character = parent
 	
-func main(delta):
+func main(delta: float):
+	# TODO: Move this character elsewhere (class should not be responsible)
 	if character.rotation_degrees != 0:
 		character.rotation_degrees = 0
 		character.global_position += Vector2(0,7)
-	Player.motion.y = gravity(delta)
-	Player.motion.x = walk(delta)
-	jump(delta)
+	Player.motion = Vector2(walk(delta), jump(delta))
 
-	
-func get_speed():
+
+func get_speed() -> int:
 	if (Player.state.movement == Enums.Movement.GRIP):
 		return GRIP_SPEED
-	else:
-		return WALK_SPEED
+	return WALK_SPEED
 
-func walk(delta):
-	var motion = Player.motion.x
+func walk(delta: float) -> int:
 	var speed = get_speed()
+	var motion = Player.motion.x
 	match Player.state.direction:
 		Enums.Direction.LEFT:
 			motion += (-speed * delta)
@@ -40,17 +38,7 @@ func walk(delta):
 			motion *= 0.95
 	return motion * 0.97
 
-#		Current speed
-#		Top speed
-#		Accel
-
-		
-func gravity(delta):
-	if (Player.state.airborn == Enums.Airborn.ON_FLOOR):
-		return 0
-	else:
-		return Player.motion.y + (GRAVITY * delta)
-
-func jump(delta):
+func jump(delta: float) -> int:
 	if (Player.state.airborn == Enums.Airborn.JUMPED):
-		Player.motion.y -= JUMP_FORCE * delta 
+		return JUMP_FORCE
+	return Player.motion.y + (GRAVITY * delta)
