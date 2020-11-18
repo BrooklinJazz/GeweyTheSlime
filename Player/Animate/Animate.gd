0ffd5a0b9d0ef2d8f2cf6707c721b653e5165303
Brooklin Myers <brooklinjazzmyers@gmail.com>
2020-11-17 21:08:36 -0700

    remove comments

--- Player/Animate/Animate.gd
+++ Player/Animate/Animate.gd
@@ -18,10 +18,3 @@ func animate_factory() -> Node:
 		return WallAnimate.new(self.body, self.legs)
 	print("ground")
 	return GroundAnimate.new(self.body, self.legs)
-
-#	if (Player.state.airborne.on_ceiling and Player.state.movement.grip):
-#		return CeilingMovement.new(self.character)
-#	elif (Player.state.airborne.on_wall and Player.state.movement.grip):
-#		return WallMovement.new(self.character)
-#	else:
-#		return GroundMovement.new(self.character)
