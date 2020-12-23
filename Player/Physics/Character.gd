c1601a1d074bfe1d5dcb6a0b5315573ba21187d3
Brooklin Myers <brooklinjazzmyers@gmail.com>
2020-12-22 23:15:12 -0700

    bugily replace physics

--- Player/Physics/Character.gd
+++ Player/Physics/Character.gd
@@ -6,20 +6,16 @@ var StateMachine
 
 func _ready():
 	Animations = PlayerAnimate.new($Sprites/Body, $Sprites/Legs)
-	Physics = PlayerPhysics.new(self)
-	StateMachine = PlayerStateMachine.new(self)
+	StateMachine = PlayerStateMachine.new()
 
 func _physics_process(delta):
-	self.update_player_state()
-	self.update_player_motion(delta)
-	self.update_player_animations()
+	StateMachine.run(self, delta)
+#	self.update_player_motion(delta)
+#	self.update_player_animations()
 
-func update_player_state():
-	Player.state = StateMachine.get_state()
-
-func update_player_motion(delta: float):
-	Player.motion = move_and_slide(Physics.get_motion(delta), Vector2.UP)
-	
-
-func update_player_animations():
-	Animations.animate()
+#func update_player_motion(delta: float):
+##	Player.motion = move_and_slide(Physics.get_motion(delta), Vector2.UP)
+#
+#
+#func update_player_animations():
+#	Animations.animate()
--- Player/StateMachine.gd
+++ Player/StateMachine.gd
@@ -1,36 +1,9 @@
 extends Node
 
-
-func StateMachineAdapter(current_state):
-	var jumped = false
-	var on_ceiling = false
-	var on_floor = false
-	var on_wall = false
-	var idle = false
-	var on_wall_left = false
-	var on_wall_right = false
-	if (current_state == States.GROUND):
-		on_floor = true
-	elif (current_state == States.JUMPED):
-		jumped = true
-	elif (current_state == States.Grabbed.LEFT_WALL):
-		on_wall = true
-		on_wall_right = true # obviously wrong and needs to be changed but works
-	elif (current_state == States.Grabbed.RIGHT_WALL):
-		on_wall = true
-		on_wall_left = true # obviously wrong and needs to be changed but works
-	elif (current_state == States.Grabbed.CEILING):
-		on_ceiling = true
-	return {
-			"jumped": jumped,
-			"on_ceiling": on_ceiling,
-			"on_floor": on_floor,
-			"on_wall": on_wall,
-			"idle": idle,
-			"on_wall_left": on_wall_left,
-			"on_wall_right": on_wall_right,
-		}
-		
+const GRAVITY = 400
+const WALK_SPEED = 400
+const JUMP_FORCE = -250.0
+const ACCELERATION = 200
 
 class State:
 	func get_event(character):
@@ -39,7 +12,46 @@ class State:
 		pass
 	func on_enter(character):
 		pass
+	func get_motion(delta: float):
+		return Vector2(0, 0)
+
+#	TODO extract these
+
+	func walk(delta: float) -> float:
+		var motion = Player.motion.x
+		if (Input.is_action_pressed("left")):
+			motion += (-WALK_SPEED * delta)
+		elif (Input.is_action_pressed("right")):
+			motion += (WALK_SPEED * delta)
+		else:
+			motion *= 0.95
+		return motion * 0.97
 	
+	func fall(delta: float) -> float:
+		return Player.motion.y + (GRAVITY * delta)
+	
+	func jump(delta: float) -> float:
+		return JUMP_FORCE
+	
+	func ceiling_climb(delta: float) -> float:
+		var motion = Player.motion.x
+		if (Input.is_action_pressed("left")):
+			motion += (-ACCELERATION * delta)
+		elif Input.is_action_pressed("right"):
+			motion += (ACCELERATION * delta)
+		else:
+			motion *= 0.95
+		return motion * 0.97
+	
+	func wall_climb(delta:float) -> float:
+		var motion = Player.motion.y
+		if (Input.is_action_pressed("up")):
+			motion += -ACCELERATION  * delta
+		elif (Input.is_action_pressed("down")):
+			motion += ACCELERATION  * delta
+		else:
+			motion *= 0.95
+		return motion * 0.97
 
 class GroundState extends State:
 	func get_event(character):
@@ -47,6 +59,13 @@ class GroundState extends State:
 			return Events.JUMP
 		elif (Input.is_action_pressed("grip")):
 			return Events.ATTACH_TO_FLOOR
+			
+	func on_enter(character):
+		character.rotation_degrees = 0
+		character.global_position += Vector2(0, 7)
+	
+	func get_motion(delta: float) -> Vector2:
+		return Vector2(walk(delta), 0)
 
 class CeilingState extends State:
 	func get_event(character):
@@ -62,6 +81,9 @@ class CeilingState extends State:
 	func on_enter(character):
 		character.rotation_degrees = 180
 		character.global_position += Vector2(0, -9)
+		
+	func get_motion(delta):
+		return Vector2(ceiling_climb(delta), -1)
 
 class GrabbedFloorState extends State:
 	func get_event(character):
@@ -73,9 +95,12 @@ class GrabbedFloorState extends State:
 			return Events.RELEASE
 
 class JumpState extends State:
+	func get_motion(delta):
+		return Vector2(walk(delta), jump(delta))
 	func get_event(character):
 		return Events.FALL
-			
+					
+
 class RightWallState extends State:
 	func get_event(character):
 		if (character.is_on_ceiling() and Input.is_action_pressed("up")):
@@ -89,8 +114,9 @@ class RightWallState extends State:
 		character.rotation_degrees = 90
 		character.global_position += Vector2(-7, 0)
 		
+	func get_motion(delta):
+		return Vector2(-1, wall_climb(delta))
 		
-
 class LeftWallState extends State:
 	func get_event(character):
 		if (character.is_on_ceiling() and Input.is_action_pressed("up")):
@@ -103,13 +129,18 @@ class LeftWallState extends State:
 	func on_enter(character):
 		character.rotation_degrees = -90
 		character.global_position += Vector2(7, 0)
-
+		
+	func get_motion(delta):
+		return Vector2(1, wall_climb(delta))
 
 class AirState extends State:
+	func get_motion(delta):
+		return Vector2(walk(delta), fall(delta))
+	
 	func get_event(character):
 		if (character.is_on_floor()):
 			return Events.LAND
-		elif (character.is_on_ceiling()):
+		elif (character.is_on_ceiling() and Input.is_action_pressed("grip")):
 			return Events.ATTACH_TO_CEILING
 		elif (character.is_on_wall() and Input.is_action_pressed("left") and Input.is_action_pressed("grip")):
 			return Events.ATTACH_TO_LEFT_WALL
@@ -118,6 +149,9 @@ class AirState extends State:
 		elif (!Input.is_action_pressed("grip")):
 			return Events.RELEASE
 
+
+class_name PlayerStateMachine
+var current_state = States.AIR
 
 var FSM = {
 	States.AIR: {
@@ -157,106 +191,101 @@ var FSM = {
 		Events.ATTACH_TO_RIGHT_WALL: States.Grabbed.RIGHT_WALL
 	}	
 }
+var state_factory = {
+	States.AIR: AirState.new(),
+	States.JUMPED: JumpState.new(),
+	States.GROUND: GroundState.new(),
+	States.Grabbed.FLOOR:  GrabbedFloorState.new(),
+	States.Grabbed.LEFT_WALL: RightWallState.new(),
+	States.Grabbed.RIGHT_WALL: LeftWallState.new(),
+	States.Grabbed.CEILING: CeilingState.new()
+}
 
-
-
-class StateMachine:
-	var current_state = States.AIR
-	var fsm
-	var state_factory = {
-		States.AIR: AirState.new(),
-		States.JUMPED: JumpState.new(),
-		States.GROUND: GroundState.new(),
-		States.Grabbed.FLOOR:  GrabbedFloorState.new(),
-		States.Grabbed.LEFT_WALL: RightWallState.new(),
-		States.Grabbed.RIGHT_WALL: LeftWallState.new(),
-		States.Grabbed.CEILING: CeilingState.new()
-	}
-	func _init(state, machine):
-		fsm = machine
-		current_state = state
-		
-	func transition(character):
-		var transitions = fsm[current_state]
-		var state = state_factory[current_state]
-		var event = state.get_event(character)
-		if event in transitions:
-			state.on_exit(event, character)
-			current_state = transitions[event]
-			state_factory[current_state].on_enter(character)
-
-
-class_name PlayerStateMachine
-var state_machine
-var character
-
-
-func _init(parent):
-	character = parent
-	state_machine = StateMachine.new(States.AIR, FSM)
 	
-
-func get_state():
-	
-	state_machine.transition(character)
-	return {
-		"airborne": StateMachineAdapter(state_machine.current_state),
-		"direction": {
-			"x": x_direction_factory(),
-			"y": y_direction_factory()
-		},
-		"movement": movement_factory()
-	}
-
-func x_direction_factory():
-	var right = false
-	var center = false
-	var left = false
-	if (Input.is_action_pressed("right") and Input.is_action_pressed("left")):
-		center = true
-	elif (Input.is_action_pressed("left")):
-		left = true
-	elif (Input.is_action_pressed("right")):
-		right = true
-	else:
-		center = true
-	return {
-		"right": right,
-		"center": center,
-		"left": left
-	}
-
-func y_direction_factory():
-	var up = false
-	var down = false
-	var center = false
-	if (Input.is_action_pressed("up") and Input.is_action_pressed("down")):
-		center = true
-	elif (Input.is_action_pressed("down")):
-		down = true
-	elif (Input.is_action_pressed("up")):
-		up = true
-	else:
-		center = true
-	return {
-		"up": up,
-		"center": center,
-		"down": down
-	}
-
-func movement_factory():
-	var charge_jump = false
-	var grip = false
-	var walk = false
-	if (Input.is_action_pressed("jump")):
-		charge_jump = true
-	elif (Input.is_action_pressed("grip")):
-		grip = true
-	else:
-		walk = true
-
-	return {
-		"charge_jump": charge_jump,
-		"grip": grip,
-		"walk": walk
-	}
+func run(character,  delta):
+	var transitions = FSM[current_state]
+	var state = state_factory[current_state]
+	var event = state.get_event(character)
+#	TODO Extract
+	Player.motion = character.move_and_slide(state.get_motion(delta), Vector2.UP)
+	print(current_state)
+	if event in transitions:
+		print(event)
+		state.on_exit(event, character)
+		current_state = transitions[event]
+		print(current_state)
+		state_factory[current_state].on_enter(character)
+#
+#class_name PlayerStateMachine
+#var state_machine
+#var character
+#
+#func _init(parent):
+#	character = parent
+#	state_machine = StateMachine.new(States.AIR, FSM)
+#
+#
+#func get_state():
+#
+#	state_machine.transition(character)
+#	return {
+#		"airborne": StateMachineAdapter(state_machine.current_state),
+#		"direction": {
+#			"x": x_direction_factory(),
+#			"y": y_direction_factory()
+#		},
+#		"movement": movement_factory()
+#	}
+#
+#func x_direction_factory():
+#	var right = false
+#	var center = false
+#	var left = false
+#	if (Input.is_action_pressed("right") and Input.is_action_pressed("left")):
+#		center = true
+#	elif (Input.is_action_pressed("left")):
+#		left = true
+#	elif (Input.is_action_pressed("right")):
+#		right = true
+#	else:
+#		center = true
+#	return {
+#		"right": right,
+#		"center": center,
+#		"left": left
+#	}
+#
+#func y_direction_factory():
+#	var up = false
+#	var down = false
+#	var center = false
+#	if (Input.is_action_pressed("up") and Input.is_action_pressed("down")):
+#		center = true
+#	elif (Input.is_action_pressed("down")):
+#		down = true
+#	elif (Input.is_action_pressed("up")):
+#		up = true
+#	else:
+#		center = true
+#	return {
+#		"up": up,
+#		"center": center,
+#		"down": down
+#	}
+#
+#func movement_factory():
+#	var charge_jump = false
+#	var grip = false
+#	var walk = false
+#	if (Input.is_action_pressed("jump")):
+#		charge_jump = true
+#	elif (Input.is_action_pressed("grip")):
+#		grip = true
+#	else:
+#		walk = true
+#
+#	return {
+#		"charge_jump": charge_jump,
+#		"grip": grip,
+#		"walk": walk
+#	}
